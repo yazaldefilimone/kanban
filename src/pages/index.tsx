a35a717c97531e9df0c17cb92a186482e949984b
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-22 11:00:48 +0200

    feat: init home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,8 @@
-import type { GetStaticProps, NextPage } from 'next';
-import { Info } from '~/domain/entities';
-import { InfoFactory } from '~/infrastructure/factories';
+import type { NextPage } from 'next';
 import { HomeTemplete } from '~/infrastructure/ui/templetes/Home';
 
-const Home: NextPage<{ data?: Info[] }> = ({ data }) => {
-  return <HomeTemplete data={data} />;
-};
-
-export const getStaticProps: GetStaticProps = async () => {
-  const infoFactory = InfoFactory();
-  const result = await infoFactory.handle();
-
-  if (result.isLeft()) {
-    return { notFound: true };
-  }
-
-  return {
-    props: {
-      data: result.value
-    }
-  };
+const Home: NextPage = () => {
+  return <HomeTemplete />;
 };
 
 export default Home;
