60960de22bdad5bb14664bbe2e1191539e4f79f8
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-30 21:54:17 +0200

    feat: create login page

---
+++ src/pages/user/login.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import { Login } from '~/infrastructure/ui/templetes/Login';
+
+const login: NextPage = () => {
+  return <Login />;
+};
+
+export default login;
