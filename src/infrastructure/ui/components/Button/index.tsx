7a83e39908aca007af0e21625540e4ce1c647aa9
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-11-02 15:03:55 +0200

    feat: add border option

--- src/infrastructure/ui/components/Button/index.tsx
+++ src/infrastructure/ui/components/Button/index.tsx
@@ -6,11 +6,12 @@ type ButtonProps = {
   action: () => void;
   children: ReactNode;
   bg?: boolean;
+  border?: boolean;
 };
 
-export const Button: FunctionComponent<ButtonProps> = ({ action, type, children, bg }) => {
+export const Button: FunctionComponent<ButtonProps> = ({ action, type, children, bg, border }) => {
   return (
-    <ButtonContainer type={type || 'button'} bg={bg === undefined ? true : bg} onClick={() => action()}>
+    <ButtonContainer type={type || 'button'} bg={bg === undefined ? true : bg} border={border || false} onClick={() => action()}>
       {children}
     </ButtonContainer>
   );
--- src/infrastructure/ui/components/Button/styles.ts
+++ src/infrastructure/ui/components/Button/styles.ts
@@ -1,17 +1,19 @@
 import styled from 'styled-components';
 
-export const ButtonContainer = styled.button<{ bg: boolean }>`
+export const ButtonContainer = styled.button<{ bg: boolean; border: boolean }>`
   padding: 0.5rem 1.2rem;
   background: ${(props) => (props.bg ? 'var(--first-color)' : 'none')};
   color: var(--text-color);
   font-size: var(--normal-font-size);
-  border-radius: 0.2rem;
+  border-radius: 0.4rem;
+  border: 2px solid ${(props) => (props.border ? 'var(--first-color)' : 'transparent')};
   font-weight: 700;
   cursor: pointer;
   transition: 0.3s;
-  min-height: 40px;
+  min-height: 45px;
   display: flex;
   align-items: center;
+  justify-content: center;
   gap: 0.3rem;
   &:hover {
     opacity: 0.9;
