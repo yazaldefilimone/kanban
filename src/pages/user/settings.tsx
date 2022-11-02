9ab87573d1122f0312b348b2d105ecf6741d49ad
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-11-02 15:06:01 +0200

    feat: create setting page

---
+++ src/pages/user/settings.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import { HomeTemplete } from '~/infrastructure/ui/templetes/Home';
+
+const Settings: NextPage = () => {
+  return <HomeTemplete />;
+};
+
+export default Settings;
