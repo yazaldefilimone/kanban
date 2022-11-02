17fdc99a6a9965f48f66a8624fdecfadc5d3ea28
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-11-02 15:06:15 +0200

    feat: create sign page

---
+++ src/pages/user/sign.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import { HomeTemplete } from '~/infrastructure/ui/templetes/Home';
+
+const Sign: NextPage = () => {
+  return <HomeTemplete />;
+};
+
+export default Sign;
