1f6f09d6ab8bbb9325105c26ea107c5027f86bac
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-11-04 23:58:55 +0200

    feat: create boardForm component

---
+++ src/infrastructure/ui/components/BoardForm/index.tsx
@@ -0,0 +1,72 @@
+import { FunctionComponent, ReactNode, useState } from 'react';
+import { Button } from '../Button';
+import { Input } from '../Input';
+import { BoardFormContainer, BoardFormContent, SubTaskFormContainer, SubTaskInput, SubtaskBtn, Subtasks } from './styles';
+import { ComboBox } from '../Combobox';
+import { StatusForm } from '../StatusForm';
+interface BoardFormProps {
+  children?: ReactNode;
+  heading: string;
+}
+
+export const BoardForm: FunctionComponent<BoardFormProps> = ({ heading }) => {
+  const initForm = {
+    name: '',
+    description: ''
+  };
+  const [form, SetForm] = useState(initForm);
+  const [subTaskCount, SetSubTaskCount] = useState<number[]>([]);
+  const [submit, SetSubmit] = useState<boolean>(false);
+
+  function handlerName(name: string) {
+    SetForm((state) => ({ ...state, name }));
+  }
+
+  function handlerBtn(id: number) {
+    if (id < 2) return;
+    SetSubTaskCount((count) => count.filter((c, index) => index !== id));
+  }
+
+  function handlerActionSubmit() {
+    SetSubmit(true);
+  }
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+
+  return (
+    <BoardFormContainer>
+      <h3>{heading}</h3>
+      <BoardFormContent>
+        <Input
+          label="Name"
+          name="name"
+          placeholder="Enter Board name"
+          action={{
+            SetValue: handlerName,
+            value: form.name
+          }}
+        />
+
+        <SubTaskFormContainer>
+          <label>Board Status</label>
+          <Subtasks>
+            {subTaskCount.map((cou, key) => (
+              <SubTaskInput key={`status-${key}`}>
+                <StatusForm submit={submit} />
+              </SubTaskInput>
+            ))}
+          </Subtasks>
+
+          <SubtaskBtn>
+            <Button action={() => SetSubTaskCount((count) => [...count, count.length])}>Add new Status</Button>
+            <Button bg={false} border={true} action={() => handlerBtn(subTaskCount.length - 1)}>
+              Remove last Status
+            </Button>
+          </SubtaskBtn>
+        </SubTaskFormContainer>
+      </BoardFormContent>
+
+      <Button action={handlerActionSubmit}>Save Board</Button>
+    </BoardFormContainer>
+  );
+};
---
+++ src/infrastructure/ui/components/BoardForm/styles.ts
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+
+export const BoardFormContainer = styled.div`
+  z-index: 20;
+  background-color: var(--gray-color);
+  padding: 2rem;
+  border-radius: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  max-height: 900px;
+  overflow-y: auto;
+  overflow-x: hidden;
+`;
+
+export const BoardFormContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 450px;
+`;
+
+export const BoardFormArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.3rem;
+
+  > label {
+    font-weight: 600;
+  }
+  > textarea {
+    min-width: 250px;
+    height: 70px;
+    background: none;
+    resize: none;
+    border: none;
+    outline: none;
+    border: 2px solid var(--border-color);
+    color: var(--text-color);
+    font-size: var(--normal-font-size);
+    padding: 0.5rem;
+    transition: 0.3s;
+    border-radius: 0.4rem;
+    &:hover {
+      border-color: var(--first-color);
+    }
+  }
+`;
+export const SubTaskInput = styled.div``;
+
+export const SubTaskFormContainer = styled.div`
+  > label {
+    font-weight: 600;
+  }
+
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const SubtaskBtn = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+export const Subtasks = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
