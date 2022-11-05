bc61b3f576d4f3911eab6c4cd35f49e9db1a6673
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-11-05 11:01:35 +0200

    feat: create http client service

---
+++ src/application/protocols/services/http.ts
@@ -0,0 +1,27 @@
+export type httpRequestType = {
+  url: string;
+  method: httpMethodType;
+  body?: any;
+  headers?: any;
+};
+
+export interface IHttpClient<R = any> {
+  request: (data: httpRequestType) => Promise<HttpResponse<R>>;
+}
+
+export type httpMethodType = 'post' | 'get' | 'put' | 'delete';
+
+export enum httpStatusCode {
+  ok = 200,
+  noContent = 204,
+  badRequest = 400,
+  unauthorized = 401,
+  forbidden = 403,
+  notFound = 404,
+  serverError = 500
+}
+
+export type HttpResponse<T = any> = {
+  statusCode: httpStatusCode;
+  body?: T;
+};
