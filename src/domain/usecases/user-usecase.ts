c651dbe016b1dc97b8b5a675980740e39d35a3dd
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-11-06 23:46:08 +0200

    fix: change type of UserUsecase Interface

--- src/domain/usecases/user-usecase.ts
+++ src/domain/usecases/user-usecase.ts
@@ -4,8 +4,8 @@ import { User } from '~/domain/entities';
 export interface IUserUseCase {
   login: (data: IUserUseCase.LoginInput) => IUserUseCase.LoginOutput;
   sign: (data: IUserUseCase.SignOutput) => IUserUseCase.SignOutput;
-  getId: (data: IUserUseCase.GetIdOutput) => IUserUseCase.GetIdOutput;
-  getToken: (data: IUserUseCase.GetTokenOutput) => IUserUseCase.GetTokenOutput;
+  getId: (data: IUserUseCase.GetIdInput) => IUserUseCase.GetIdOutput;
+  getToken: (data: IUserUseCase.GetTokenInput) => IUserUseCase.GetTokenOutput;
 }
 
 type successUser = { token: string; id: string };
@@ -16,6 +16,6 @@ export namespace IUserUseCase {
   export type SignOutput = Promise<Either<string, successUser>>;
   export type GetIdInput = { id: string };
   export type GetIdOutput = Promise<Either<string, User>>;
-  export type GetTokenInput = { id: string };
+  export type GetTokenInput = { token: string };
   export type GetTokenOutput = Promise<Either<string, User>>;
 }
