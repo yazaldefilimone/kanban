fb6a5cbe92af08a5fabb71b9cb7921aa09e7c9f6
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-11-07 12:40:27 +0200

    feat: pass usecase

--- src/pages/user/login.tsx
+++ src/pages/user/login.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from 'next';
+import { userFactory } from '~/infrastructure/factories';
 import { Login } from '~/infrastructure/ui/templetes/Login';
 
 const login: NextPage = () => {
-  return <Login />;
+  const factory = userFactory();
+
+  return <Login userUseCase={factory} />;
 };
 
 export default login;
