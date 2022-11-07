5d6ef9795af49b497a60ddab5dbf9f0827265f5c
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-11-07 12:42:14 +0200

    feat: using axios

--- src/infrastructure/services/http/index.ts
+++ src/infrastructure/services/http/index.ts
@@ -1,17 +1,22 @@
+import axios, { AxiosResponse } from 'axios';
 import { httpRequestType, HttpResponse, IHttpClient } from '~/application/protocols/services';
 
 export class HttpClient<R = any> implements IHttpClient {
   async request(data: httpRequestType): Promise<HttpResponse<R>> {
-    const body: ReadableStream<Uint8Array> = data.body ? (JSON.stringify(data.body) as any) : null;
-
-    const response = await fetch(data.url, {
-      method: data.method.toLocaleUpperCase(),
-      headers: data.headers,
-      body
-    });
-
-    const result = await response.json();
-
-    return result;
+    let axiosResponse: AxiosResponse;
+    try {
+      axiosResponse = await axios.request({
+        url: data.url,
+        method: data.method,
+        data: data.body,
+        headers: data.headers
+      });
+    } catch (error: any) {
+      axiosResponse = error.response;
+    }
+    return {
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data
+    };
   }
 }
