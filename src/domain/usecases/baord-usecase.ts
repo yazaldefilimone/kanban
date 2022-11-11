4294825ef6a95c46175ec9630bd68378db4b89ab
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-11-11 22:00:46 +0200

    fix: create type namespace for deleteInput

--- src/domain/usecases/baord-usecase.ts
+++ src/domain/usecases/baord-usecase.ts
@@ -3,7 +3,7 @@ import { Board } from '~/domain/entities';
 
 export interface IBoardUseCase {
   create: (data: IBoardUseCase.createInput) => IBoardUseCase.createOutput;
-  delete: (data: IBoardUseCase.deleteOutput) => IBoardUseCase.deleteOutput;
+  delete: (data: IBoardUseCase.deleteInput) => IBoardUseCase.deleteOutput;
   getId: (data: IBoardUseCase.getIdInput) => IBoardUseCase.getIdOutput;
   getUserId: (data: IBoardUseCase.getUserIdInput) => IBoardUseCase.getUserIdOutput;
 }
@@ -13,6 +13,7 @@ export namespace IBoardUseCase {
   export type createInput = Omit<successBoard, 'id'>;
   export type createOutput = Promise<Either<string, successBoard>>;
   export type deleteOutput = Promise<Either<string, successBoard>>;
+  export type deleteInput = { id: string };
   export type getIdInput = { id: string };
   export type getIdOutput = Promise<Either<string, Board>>;
   export type getUserIdInput = { id: string };
